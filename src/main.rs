9da39ab4e5e7ba368b52bf384e6556c1b6e408b4
VimFreeman <github@freiman.cc>
2024-01-19 23:52:08 +0000

    Add sleep to loop to not hog CPU

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ mod scheduler;
 
 extern crate ll_protocol;
 
+use std::{thread, time};
+
 fn main() {
   loop {
     println!("THIS IS A TEST FLIGHTCODE LOOP");
+    thread::sleep(time::Duration::from_millis(1000));
   }
 }
