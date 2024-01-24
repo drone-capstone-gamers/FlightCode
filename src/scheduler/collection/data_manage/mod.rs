e90b38ac25b518ddb4b8408457d103a0998be790
William Wells <williamwells2013@hotmail.com>
2024-01-23 22:45:41 -0500

    Add flightcode parameters via system environment variables, and use them for data storage location

--- src/scheduler/collection/data_manage/mod.rs
+++ src/scheduler/collection/data_manage/mod.rs
@@ -3,7 +3,14 @@ use std::{fs, thread};
 use std::path::Path;
 use std::time::Duration;
 use chrono::{DateTime, Local};
+use envconfig::Envconfig;
 use json::JsonValue;
+
+#[derive(Envconfig)]
+struct DataStorageConfig {
+    #[envconfig(from = "FLIGHTCODE_DATA_STORAGE_DIR", default = "./collected_data/")]
+    pub target_path: String
+}
 
 #[derive(PartialEq)]
 pub enum DataSource {
@@ -52,6 +59,10 @@ pub fn spawn_data_manager(data_receiver: Receiver<IncomingData>) {
 fn data_manager_loop(data_receiver: Receiver<IncomingData>) {
     let mut geotag: Option<JsonValue> = None;
 
+    let data_storage_config = DataStorageConfig::init_from_env().unwrap();
+
+    let storage_parent_dir = data_storage_config.target_path;
+
     loop {
         let data_result = data_receiver.recv_timeout(Duration::from_millis(1));
         if data_result.is_ok() {
@@ -63,17 +74,21 @@ fn data_manager_loop(data_receiver: Receiver<IncomingData>) {
             }
 
             let source = get_data_source_string(incoming_data.source);
+            let source_dir = format!("{}/{}", storage_parent_dir, source);
 
-            if !Path::new(&source).exists() {
-                fs::create_dir(source.clone()).expect(&*format!("Failed to create directory: {}", source.clone()));
+            if !Path::new(&source_dir).exists() {
+                fs::create_dir_all(&source_dir).expect(&*format!("Failed to create directory: {}", source_dir));
 
             }
 
-            let full_path = format!("{}/{}", source, &*incoming_data.time_stamp.to_rfc3339());
+            let full_path = format!("{}/{}", &source_dir, &*incoming_data.time_stamp.to_rfc3339());
 
             /**
                 TODO: Better setup file IO to write json data from each source into single file for given mission,
                       rather than potentially creating hundreds of different files rapidly
+
+                TODO: Check remaining space on storage drive of destination directory to ensure sufficient storage space
+                      and warn on running low
             */
             if incoming_data.serialized.is_some() {
                 let file = full_path.clone() + ".json";
