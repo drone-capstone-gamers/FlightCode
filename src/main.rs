f6fe8f353d54420a31db943a4a6b7cf202260062
William Wells <williamwells2013@hotmail.com>
2024-01-24 22:12:12 +0000

    Add sleep to main loop to avoid CPU hog

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ mod scheduler;
 
 extern crate ll_protocol;
 
+use std::{thread, time};
+
 fn main() {
     scheduler::start_collection_tasks();
 
-    loop {}
+    loop {
+        thread::sleep(time::Duration::from_millis(1000));
+    }
 }
