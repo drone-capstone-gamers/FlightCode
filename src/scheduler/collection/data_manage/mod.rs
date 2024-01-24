593bf33d758be39923a67af86d8f2b1c580bc7a8
William Wells <williamwells2013@hotmail.com>
2024-01-24 00:32:31 -0500

    Heavy WIP in storage manager writing source data into single file with single OS open req

--- src/scheduler/collection/data_manage/mod.rs
+++ src/scheduler/collection/data_manage/mod.rs
@@ -1,10 +1,10 @@
 use std::sync::mpsc::Receiver;
 use std::{fs, thread};
+use std::io::{Seek, SeekFrom, Write};
 use std::path::Path;
-use std::time::Duration;
 use chrono::{DateTime, Local};
 use envconfig::Envconfig;
-use json::JsonValue;
+use json::{JsonValue, object};
 
 #[derive(Envconfig)]
 struct DataStorageConfig {
@@ -50,6 +50,13 @@ impl IncomingData {
     }
 }
 
+fn create_serialized_file(parent_dir: String, source: DataSource) -> fs::File {
+    let source = get_data_source_string(source);
+
+    return fs::File::create(format!("{}/{}.json", &parent_dir, &source))
+        .expect(&*format!("Creation of {}/{}.json failed!", parent_dir, source));
+}
+
 pub fn spawn_data_manager(data_receiver: Receiver<IncomingData>) {
     thread::spawn(|| {
         data_manager_loop(data_receiver);
@@ -61,10 +68,19 @@ fn data_manager_loop(data_receiver: Receiver<IncomingData>) {
 
     let data_storage_config = DataStorageConfig::init_from_env().unwrap();
 
-    let storage_parent_dir = data_storage_config.target_path;
+    let storage_dir = format!("{}/{}", data_storage_config.target_path, Local::now().to_rfc3339());
+    if !Path::new(&storage_dir).exists() {
+        fs::create_dir_all(&storage_dir).expect(&*format!("Failed to create directory: {}", &storage_dir));
+    }
+
+    // TODO: ABSTRACT LIVE JSON WRITING TO JSON AS IO STREAM TO MODULE AND FIX
+    let mut serialized_file = create_serialized_file(storage_dir.clone(), DataSource::Example);
+
+    // Setup json file with { at start, && are dummy characters to be removed further down
+    serialized_file.write("{\"data_stream\": [\n&&".as_bytes()).expect("Failed to initialize data capture fail for unknown reason");
 
     loop {
-        let data_result = data_receiver.recv_timeout(Duration::from_millis(1));
+        let data_result = data_receiver.recv();
         if data_result.is_ok() {
             let incoming_data = data_result.unwrap();
 
@@ -74,14 +90,6 @@ fn data_manager_loop(data_receiver: Receiver<IncomingData>) {
             }
 
             let source = get_data_source_string(incoming_data.source);
-            let source_dir = format!("{}/{}", storage_parent_dir, source);
-
-            if !Path::new(&source_dir).exists() {
-                fs::create_dir_all(&source_dir).expect(&*format!("Failed to create directory: {}", source_dir));
-
-            }
-
-            let full_path = format!("{}/{}", &source_dir, &*incoming_data.time_stamp.to_rfc3339());
 
             /**
                 TODO: Better setup file IO to write json data from each source into single file for given mission,
@@ -91,19 +99,32 @@ fn data_manager_loop(data_receiver: Receiver<IncomingData>) {
                       and warn on running low
             */
             if incoming_data.serialized.is_some() {
-                let file = full_path.clone() + ".json";
-                fs::write(file.clone(), incoming_data.serialized.unwrap().to_string())
-                    .expect(&*format!("Failed to write to file: {}", file));
+                let serialized = object!{
+                    timestamp: incoming_data.time_stamp.to_rfc3339(),
+                    data: incoming_data.serialized.unwrap()
+                };
+
+                // TODO: ABSTRACT LIVE JSON WRITING TO JSON AS IO STREAM TO MODULE AND FIX
+                // Always place ]} at end to maintain file as valid json during write
+                let serialized_string = format!("{}\n", serialized.to_string()) + "]}";
+
+                // Seek back by 2 characters in json in order to overwrite } at end that maintains it as a valid fmt
+                let overwrite_json_end_offset = serialized_file.stream_position().unwrap() - 2;
+                serialized_file.seek(SeekFrom::Start(overwrite_json_end_offset)).expect("TODO: panic message");
+
+                serialized_file.write(serialized_string.as_bytes())
+                    .expect(&*format!("Failed to append to file: {source}"));
+                // TODO: ABSTRACT LIVE JSON WRITING TO JSON AS IO STREAM TO MODULE AND FIX
             }
 
             if incoming_data.file.is_some() {
-                if geotag.is_some() {
-                    // TODO: Geotag if file is image
-                }
-
-                let file = full_path.clone();
-                fs::write(file.clone(), incoming_data.file.unwrap())
-                    .expect(&*format!("Failed to write to file: {}", file));
+                // if geotag.is_some() {
+                //     // TODO: Geotag if file is image
+                // }
+                //
+                // let file = &storage_dir;
+                // fs::write(file.clone(), incoming_data.file.unwrap())
+                //     .expect(&*format!("Failed to write to file: {}", file));
             }
         }
     }
