38555804defedc30338472ce80fb3aea28e8e9ef
William Wells <williamwells2013@hotmail.com>
2024-03-04 02:33:42 +0000

    Change flightcode exec to debug

--- exec-flightcode.sh
+++ exec-flightcode.sh
@@ -1,1 +1,1 @@
-source /home/firedrone/FlightCode/config.env && /home/firedrone/FlightCode/target/release/flightcode
+source /home/firedrone/FlightCode/config.env && /home/firedrone/FlightCode/target/debug/flightcode
