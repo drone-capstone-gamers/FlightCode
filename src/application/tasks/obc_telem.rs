b1189950a9561eb6305222ac2d72f1303e5cda5e
William Wells <williamwells2013@hotmail.com>
2024-03-10 22:12:01 -0400

    Add storage space to obc telemetry

--- src/application/tasks/obc_telem.rs
+++ src/application/tasks/obc_telem.rs
@@ -1,7 +1,5 @@
 use std::process::Command;
 use std::sync::mpsc::SyncSender;
-use std::thread;
-use std::time::Duration;
 use json::object;
 use crate::application::data_manage::{DataSource, get_data_source_string, IncomingData};
 use crate::application::DataCollector;
@@ -9,6 +7,61 @@ use crate::application::timer::TimedTask;
 
 pub struct ObcTelem {
     storage_sender: SyncSender<IncomingData>
+}
+
+impl ObcTelem {
+    fn get_temperature() -> String {
+        let temp_proc_result = Command::new("vcgencmd")
+            .arg("measure_temp")
+            .output();
+
+        if temp_proc_result.is_err() {
+            println!("Failed to call cmd to measure OBC temp!");
+            return "".to_string();
+        }
+        let temp_proc = temp_proc_result.unwrap();
+
+        let temp_string: String = temp_proc.stdout.iter().map(|&c| c as char).collect();
+
+        let temp_string_split: Vec<&str> = temp_string.split("=").collect();
+        if temp_string_split.len() != 2 {
+            println!("Failed to parse command response for OBC temp!");
+            return "".to_string();
+        }
+
+        return temp_string_split[1].strip_suffix("'C\n").unwrap().to_string();
+    }
+
+    fn get_storage_left() -> String {
+        let storage_proc_result = Command::new("df")
+            .arg("-h")
+            .arg("/")
+            .output();
+
+        if storage_proc_result.is_err() {
+            println!("Failed to call cmd to check storage space!");
+            return "".to_string();
+        }
+        let storage_proc = storage_proc_result.unwrap();
+
+        let storage_output: String = storage_proc.stdout.iter().map(|&c| c as char).collect();
+
+        let storage_split: Vec<&str> = storage_output.split("\n").collect();
+
+        if storage_split.len() < 2 {
+            println!("Failed to parse command response for OBC storage!");
+            return "".to_string();
+        }
+
+        let storage_split: Vec<&str> = storage_split[1].split_whitespace().collect();
+
+        if storage_split.len() < 6 {
+            println!("Failed to parse command response for OBC storage!");
+            return "".to_string();
+        }
+
+        return format!("{}/{}", storage_split[2], storage_split[1]);
+    }
 }
 
 impl DataCollector for ObcTelem {
@@ -21,29 +74,13 @@ impl DataCollector for ObcTelem {
 
 impl TimedTask for ObcTelem {
     fn execute(&mut self) -> () {
-        let temp_proc_result = Command::new("vcgencmd")
-            .arg("measure_temp")
-            .output();
+        let temp_value = ObcTelem::get_temperature();
 
-        if temp_proc_result.is_err() {
-            println!("Failed to call cmd to measure OBC temp!");
-            thread::sleep(Duration::from_secs(10)); // TODO: only log every 10 secs but still reattempt as normal
-            return;
-        }
-        let temp_proc = temp_proc_result.unwrap();
-
-        let temp_string: String = temp_proc.stdout.iter().map(|&c| c as char).collect();
-
-        let temp_string_split: Vec<&str> = temp_string.split("=").collect();
-        if temp_string_split.len() != 2 {
-            println!("Failed to parse command response for OBC temp!");
-            return;
-        }
-
-        let temp_value = temp_string_split[1].strip_suffix("'C\n");
+        let storage_space_remaining = ObcTelem::get_storage_left();
 
         let telem_json = object!{
-            core_temperature: temp_value
+            core_temperature: temp_value,
+            storage_space: storage_space_remaining
         };
 
         let obc_telem = IncomingData::new(DataSource::ObcTelemetry, Option::from(telem_json), None);
